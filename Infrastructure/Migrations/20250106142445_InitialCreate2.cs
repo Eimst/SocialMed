6c0863266cf7ee58f37188036cfb724e3114f4bf
Eimst <sutkaitisee@gmail.com>
2025-01-10 13:05:02 +0200

    Added migrations folder

---
+++ Infrastructure/Migrations/20250106142445_InitialCreate2.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "EncryptedMessage",
+                table: "Messages",
+                newName: "EncryptedMessageForSender");
+
+            migrationBuilder.AddColumn<string>(
+                name: "EncryptedMessageForReceiver",
+                table: "Messages",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EncryptedMessageForReceiver",
+                table: "Messages");
+
+            migrationBuilder.RenameColumn(
+                name: "EncryptedMessageForSender",
+                table: "Messages",
+                newName: "EncryptedMessage");
+        }
+    }
+}
